bcf57d4e18628e3abacee9f6396dc7b1087c03c2
Yusuf Birader <yusuf.birader@hotmail.com>
2024-05-14 19:00:10 +0100

    add failing acceptance test

--- bin/test_conway.rb
+++ bin/test_conway.rb
@@ -10,4 +10,22 @@ describe "Conway" do
     driver = Driver::CLI.new(input_pattern, 3, File.read(input_pattern), bin_path)
     Specification::PlayGame.new(driver).start
   end
+
+  it "should play with blinker pattern" do
+    skip "todo"
+
+    input_pattern = File.expand_path("../testdata/blinker.rle", __dir__)
+    bin_path = File.expand_path("../bin/conway", __dir__)
+    output_pattern = <<~BLINKER
+    #N Blinker
+    #O John Conway
+    #C A period 2 oscillator that is the smallest and most common oscillator.
+    #C www.conwaylife.com/wiki/index.php?title=Blinker
+    x = 1, y = 3, rule = B3/S23
+    1o$1o$1o!
+    BLINKER
+
+    driver = Driver::CLI.new(input_pattern, 2, output_pattern, bin_path)
+    Specification::PlayGame.new(driver).start
+  end
 end
--- lib/conway/cli.rb
+++ lib/conway/cli.rb
@@ -1,8 +1,8 @@
 module Conway
   class CLI
     def start(args)
-      block_pattern_path = File.expand_path("../../testdata/block.rle", __dir__)
-      puts File.read(block_pattern_path)
+      seed_path, generations = args
+      puts File.read(seed_path)
     end
   end
 end
