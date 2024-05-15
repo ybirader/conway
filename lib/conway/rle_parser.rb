5687635a6ee44c817ffe498f54439730cca0d374
Yusuf Birader <yusuf.birader@hotmail.com>
2024-05-15 20:07:04 +0100

    refactor: make read file private

--- lib/conway/rle_parser.rb
+++ lib/conway/rle_parser.rb
@@ -2,19 +2,23 @@ module Conway
   class RleParser
     RLE_EXTENSION = ".rle"
 
-    def self.parse(path)
-      content = read_file(path)
-      content.split("\n").reject { |line| line.start_with?("#") }.join("\n")
-    end
+    class << self
 
-    def self.read_file(path)
-      raise Conway::InvalidFileError.new("must be a valid .rle file. #{path} is not.") unless File.extname(path) == RLE_EXTENSION
-
-      begin
-        File.read(path)
-      rescue Errno::ENOENT
-        raise Conway::InvalidFileError.new("must be a valid .rle file. #{path} is not.")
+      def parse(path)
+        content = read_file(path)
+        content.split("\n").reject { |line| line.start_with?("#") }.join("\n")
       end
+
+      private
+        def read_file(path)
+          raise Conway::InvalidFileError.new("must be a valid .rle file. #{path} is not.") unless File.extname(path) == RLE_EXTENSION
+
+          begin
+            File.read(path)
+          rescue Errno::ENOENT
+            raise Conway::InvalidFileError.new("must be a valid .rle file. #{path} is not.")
+          end
+        end
     end
   end
 end
--- test/test_rle_parser.rb
+++ test/test_rle_parser.rb
@@ -3,20 +3,6 @@ require "conway/rle_parser"
 
 describe "RLE Parser" do
   test_file = File.expand_path("../testdata/block.rle", __dir__)
-  it "should read an rle file" do
-    content = Conway::RleParser.read_file(test_file)
-    want = File.read(test_file)
-
-    expect(content).must_equal(want)
-  end
-
-  it "should raise an invalid file exception for an invalid path" do
-    expect(-> { Conway::RleParser.read_file("hello.rle") }).must_raise(Conway::InvalidFileError)
-  end
-
-  it "should raise an invalid file exception for a invalid extension" do
-    expect(-> { Conway::RleParser.read_file(File.expand_path("../testdata/wrong.txt", __dir__)) }).must_raise(Conway::InvalidFileError)
-  end
 
   it "should ignore comment lines" do
     content = Conway::RleParser.parse(test_file)
@@ -26,5 +12,12 @@ describe "RLE Parser" do
     CONTENT
 
     expect(content.chomp).must_equal(want.chomp)
+  end
+  it "should raise an invalid file exception for an invalid path" do
+    expect(-> { Conway::RleParser.parse("hello.rle") }).must_raise(Conway::InvalidFileError)
+  end
+
+  it "should raise an invalid file exception for a invalid extension" do
+    expect(-> { Conway::RleParser.parse(File.expand_path("../testdata/wrong.txt", __dir__)) }).must_raise(Conway::InvalidFileError)
   end
 end
