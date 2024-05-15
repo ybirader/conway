450987b909a035049e04af9bce27ecf74691c260
Yusuf Birader <yusuf.birader@hotmail.com>
2024-05-15 17:52:34 +0100

    refactor: extract methods and constants

--- lib/conway/universe.rb
+++ lib/conway/universe.rb
@@ -1,32 +1,29 @@
 module Conway
   class Universe
+    DEAD_CELL = "."
+    LIVE_CELL = "#"
+
     def initialize(width, height, seed_pattern)
       @grid = initialize_universe(width, height, seed_pattern)
     end
 
     def tick
-      grid.length.times do |row|
-        grid[0].length.times do |col|
-          cell = grid[row][col]
+      grid.each_with_index do |row, row_idx|
+        row.each_with_index do |cell, col_idx|
           if cell != "."
-            live_neighbours_count = count_live_neighbours(row, col)
-            grid[row][col] = "." if live_neighbours_count < 2
+            live_neighbours_count = count_live_neighbours(row_idx, col_idx)
+            grid[row_idx][col_idx] = DEAD_CELL if live_neighbours_count < 2
           end
         end
       end
     end
 
     def count_live_neighbours(row, col)
-      result = 0
       directions = [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [-1, -1], [-1, 1], [1, -1]]
 
-      directions.each do |direction|
-        if row + direction[0] >= 0 && row + direction[0] < grid.length && col + direction[1] >= 0 && col + direction[1] < grid[0].length && grid[row+direction[0]][col+direction[1]] != "."
-          result += 1
-        end
+      directions.count do |direction|
+        live_cell?(row+direction[0], col+direction[1])
       end
-
-      result
     end
 
     def to_s
@@ -42,14 +39,34 @@ module Conway
         result = []
 
         height.times do
-          result.push(Array.new(width).map { |_| "." })
+          result.push(Array.new(width).map { |_| DEAD_CELL })
         end
 
         seed_pattern.each do |cell|
-          result[cell[0]][cell[1]] = "#"
+          result[cell[0]][cell[1]] = LIVE_CELL
         end
 
         result
+      end
+
+      def width
+        grid[0].length
+      end
+
+      def height
+        grid.length
+      end
+
+      def live_cell?(row, col)
+         inbounds?(row, col) && !dead_cell?(row, col)
+      end
+
+      def dead_cell?(row, col)
+        grid[row][col] == DEAD_CELL
+      end
+
+      def inbounds?(row, col)
+        row >= 0 && row < height && col >= 0 && col < width
       end
   end
 end
