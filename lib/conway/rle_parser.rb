704335724f0126579e300c20f01d89f0158ca1a8
Yusuf Birader <yusuf.birader@hotmail.com>
2024-05-15 20:05:22 +0100

    should ignore comment lines

--- lib/conway/rle_parser.rb
+++ lib/conway/rle_parser.rb
@@ -2,6 +2,11 @@ module Conway
   class RleParser
     RLE_EXTENSION = ".rle"
 
+    def self.parse(path)
+      content = read_file(path)
+      content.split("\n").reject { |line| line.start_with?("#") }.join("\n")
+    end
+
     def self.read_file(path)
       raise Conway::InvalidFileError.new("must be a valid .rle file. #{path} is not.") unless File.extname(path) == RLE_EXTENSION
 
--- test/test_rle_parser.rb
+++ test/test_rle_parser.rb
@@ -17,4 +17,14 @@ describe "RLE Parser" do
   it "should raise an invalid file exception for a invalid extension" do
     expect(-> { Conway::RleParser.read_file(File.expand_path("../testdata/wrong.txt", __dir__)) }).must_raise(Conway::InvalidFileError)
   end
+
+  it "should ignore comment lines" do
+    content = Conway::RleParser.parse(test_file)
+    want = <<~CONTENT
+    x = 2, y = 2, rule = B3/S23\r
+    2o$2o!
+    CONTENT
+
+    expect(content.chomp).must_equal(want.chomp)
+  end
 end
