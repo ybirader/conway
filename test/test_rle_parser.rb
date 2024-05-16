6375f1cb91e128962d2cd434eea5e2bd5b970264
Yusuf Birader <yusuf.birader@hotmail.com>
2024-05-16 18:30:09 +0100

    refactor: use table driven test

--- test/test_rle_parser.rb
+++ test/test_rle_parser.rb
@@ -3,11 +3,15 @@ require "conway/rle_parser"
 
 describe "RLE Parser" do
   test_file = File.expand_path("../testdata/block.rle", __dir__)
-  it "should parse rle pattern file into a set of active cells relative to (0, 0), ignoring comments" do
-    content = Conway::RleParser.parse(test_file)
-    want = Set.new([[0, 0], [0, 1], [1, 0], [1, 1]])
 
-    expect(content).must_equal(want)
+  tests = [
+    { input_file: test_file, want: Set.new([[0, 0], [0, 1], [1, 0], [1, 1]])}
+  ]
+
+  tests.each do |test|
+    it "should parse rle pattern file into a set of active cells relative to (0, 0), ignoring comments" do
+      expect(Conway::RleParser.parse(test[:input_file])).must_equal(test[:want])
+    end
   end
   it "should raise an invalid file exception for an invalid path" do
     expect(-> { Conway::RleParser.parse("hello.rle") }).must_raise(Conway::InvalidFileError)
