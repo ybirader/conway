d52c0dbfb0a5bf453509afb6039dc0862fb523fe
Yusuf Birader <yusuf.birader@hotmail.com>
2024-05-19 11:59:59 +0100

    add glider acceptance test

--- bin/test_conway.rb
+++ bin/test_conway.rb
@@ -18,4 +18,12 @@ describe "Conway" do
     driver = Driver::CLI.new(input_pattern, 1, "o$o$o!\n", bin_path)
     Specification::PlayGame.new(driver).start
   end
+
+  it "should play with glider pattern" do
+    input_pattern = File.expand_path("../testdata/glider.rle", __dir__)
+    bin_path = File.expand_path("../bin/conway", __dir__)
+
+    driver = Driver::CLI.new(input_pattern, 3, "o2b$b2o$2ob!\n", bin_path)
+    Specification::PlayGame.new(driver).start
+  end
 end
