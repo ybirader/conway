1661e7a3455d0e73aeb40472cdf2e711a8f4b7c9
Yusuf Birader <yusuf.birader@hotmail.com>
2024-05-19 18:24:45 +0100

    refactor: extract method

--- lib/conway/rle_serializer.rb
+++ lib/conway/rle_serializer.rb
@@ -14,15 +14,10 @@ module Conway
 
     def serialize
       min_row_cell, max_row_cell = row_boundaries
-      min_col_cell, max_col_cell = col_boundaries
-
       result = []
 
       min_row_cell.upto(max_row_cell) do |row_idx|
-        row = ""
-        min_col_cell.upto(max_col_cell) do |col_idx|
-          row += cells.include?([row_idx, col_idx]) ? LIVE_CELL : DEAD_CELL
-        end
+        row = symbolize_row(row_idx)
         result.push(encode(row))
       end
 
@@ -32,12 +27,13 @@ module Conway
     private
       attr_reader :cells
 
-      def row_boundaries
-        cells.minmax { |a, b| a[0] <=> b[0] }.map { |cell| cell[0] }
-      end
-
-      def col_boundaries
-        cells.minmax { |a, b| a[1] <=> b[1] }.map { |cell| cell[1] }
+      def symbolize_row(row_idx)
+        min_col_cell, max_col_cell = col_boundaries
+        row = ""
+        min_col_cell.upto(max_col_cell) do |col_idx|
+          row += cells.include?([row_idx, col_idx]) ? LIVE_CELL : DEAD_CELL
+        end
+        row
       end
 
       def encode(pattern_string)
@@ -55,6 +51,14 @@ module Conway
         end
         result += count > 1 ? "#{count}#{current_char}" : current_char
         result
+      end
+
+      def row_boundaries
+        cells.minmax { |a, b| a[0] <=> b[0] }.map { |cell| cell[0] }
+      end
+
+      def col_boundaries
+        cells.minmax { |a, b| a[1] <=> b[1] }.map { |cell| cell[1] }
       end
   end
 end
